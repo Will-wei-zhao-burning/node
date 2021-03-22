ba1a70ab1a1a92e34ff85b83d68483d039a11dff
Will <775817124@qq.com>
2021-03-22 14:06:07 +0800

    first init

---
+++ 02-给node传递参数/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: BurNing
+ * @Date: 2021-03-21 16:53:31
+ * @LastEditors: BurNing
+ * @LastEditTime: 2021-03-21 17:01:20
+ */
+console.log('hello world!');
+console.log(process.argv);
---
+++ 03_全局对象/全局对象.js
@@ -0,0 +1,10 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: BurNing
+ * @Date: 2021-03-21 17:28:18
+ * @LastEditors: BurNing
+ * @LastEditTime: 2021-03-21 17:30:41
+ */
+console.log(__dirname);
+console.log(__filename);
